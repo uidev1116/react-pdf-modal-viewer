d6bc928ea4ec6344608f5d46ba3555b427a17299
ui rikuto <mdr.crew.1219@gmail.com>
2022-07-18 04:41:59 +0900

    create development environment

---
+++ .eslintrc.cjs
@@ -0,0 +1,47 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: ['examples/src/**'],
+        peerDependencies: true,
+      },
+    ],
+    'import/extensions': [
+        'error',
+        {
+          js: 'never',
+          jsx: 'never',
+          ts: 'never',
+          tsx: 'never',
+        },
+      ],
+    'react/jsx-filename-extension': [
+      'error',
+      {
+        extensions: ['.jsx', '.tsx'],
+      },
+    ],
+    'react/react-in-jsx-scope': 'off'
+  },
+  settings: {
+    'import/resolver': {
+      vite: {},
+      typescript: {},
+    },
+  },
+};
---
+++ vite.config.ts
@@ -0,0 +1,37 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import eslint from 'vite-plugin-eslint'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.tsx'),
+      name: 'ReactPDFViewer',
+      fileName: 'react-pdf-viewer'
+    },
+    rollupOptions: {
+      external: ['react'],
+      output: {
+        globals: {
+          react: 'React'
+        }
+      }
+    }
+  },
+  server: {
+    open: '/examples/index.html'
+  },
+  plugins: [
+    react(),
+    eslint({
+      extensions: ['js', 'ts', 'tsx'],
+      exclude: ['/node_modules/'],
+      emitError: true,
+      emitWarning: true,
+      failOnError: true,
+      fix: true,
+    })
+  ]
+})
