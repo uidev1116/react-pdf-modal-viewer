c0d2ef08e84e7469b1bff8c666c5bef6e835904d
uidev1116 <uidev1116@gmail.com>
2022-08-19 02:51:00 +0900

    build: add eslint config for react hooks

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,7 +3,12 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['plugin:react/recommended', 'airbnb', 'prettier'],
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'airbnb/hooks',
+    'prettier'
+  ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
