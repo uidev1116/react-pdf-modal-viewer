9e8ec4768322e5751416c7db3f3b672f4417ed6b
uidev1116 <uidev1116@gmail.com>
2022-08-24 02:26:18 +0900

    fix(useBodyScrollLock): クリーンアップ関数が、コンポーネントのアンマウント時にも動作してしまう問題を修正

--- src/hooks/useBodyScrollLock.ts
+++ src/hooks/useBodyScrollLock.ts
@@ -43,9 +43,12 @@ export const useBodyScrollLock = (): UseBodyScrollLockReturn => {
     }
   }, [])
 
-  useEffect(() => () => {
-    clearAllBodyScrollLocks()
-  })
+  useEffect(
+    () => () => {
+      clearAllBodyScrollLocks()
+    },
+    []
+  )
 
   const setRef = useCallback((node: HTMLElement | null) => {
     if (node === null) {
