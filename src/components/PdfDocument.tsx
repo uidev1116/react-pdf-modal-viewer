649a62110d5028f1b0f3fe5e88465bba48986654
uidev1116 <uidev1116@gmail.com>
2023-01-04 05:05:31 +0900

    refactor: 型定義をリファクタリング

--- src/components/PdfDocument.tsx
+++ src/components/PdfDocument.tsx
@@ -4,18 +4,16 @@ import { Document } from 'react-pdf'
 import { useViewer } from '../hooks'
 
 import type { ReactNode } from 'react'
-import type {
-  PDFDocumentProxy,
-  DocumentInitParameters,
-} from 'pdfjs-dist/types/src/display/api'
+import type { PDFDocumentProxy } from 'pdfjs-dist/types/src/display/api'
 
 import type { ViewerProps } from './Viewer'
 
 export interface PdfDocumentProps
-  extends Pick<ViewerProps, 'error' | 'loading' | 'noData'> {
-  file: string | Uint8Array
+  extends Pick<
+    ViewerProps,
+    'file' | 'options' | 'error' | 'loading' | 'noData'
+  > {
   className?: string
-  options?: DocumentInitParameters
   children?: ReactNode
 }
 
